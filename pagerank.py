08c9b8d6237f57bbee022afc921d644137716e90
KathrynWoest <kathryn.woest@gmail.com>
2026-04-08 10:28:22 -0700

    Establish algorithm for page rank distribution

--- pagerank.py
+++ pagerank.py
@@ -37,18 +37,32 @@ def calculate(graph, output_file):
     num_nodes = len(graph.nodes())
     initial_pr = 1/num_nodes
 
-    # create a copy of the graph to track changes and assign each node with the initial page rank
-    nx.set_node_attributes(graph, 0, name="page_rank")
-    newpr_graph = copy.deepcopy(graph)
+    # initialize round 0, where the page rank of each node is the initial page rank calculated above
     nx.set_node_attributes(graph, initial_pr, name="page_rank")
+    newpr_graph = copy.deepcopy(graph)
 
-    for node, rank in graph.nodes(data="page_rank"):
-        edges = list(graph.out_edges(node))
-        if edges:
-            shared_pr = rank/len(edges)
-            for u, v in edges:
-                newpr_graph.nodes[v]["page_rank"] = newpr_graph.nodes[v].get("page_rank", 0) + shared_pr
-        else:
-            newpr_graph.nodes[node]["page_rank"] = rank
+    # loop through the algorithm for the rest of the rounds
+    for i in range(100):
+        # create a copy of the graph from the previous round to use for page rank distribution, and then reset the current graph
+        last_step_graph = copy.deepcopy(newpr_graph)
+        nx.set_node_attributes(newpr_graph, 0, name="page_rank")
 
+        # for every node in the graph
+        for node, rank in last_step_graph.nodes(data="page_rank"):
+            # calculate all of the out-edges
+            edges = list(last_step_graph.out_edges(node))
+            # if the node has out-edges, calculate the amount of page rank to distribute to each outgoing node
+            if edges:
+                shared_pr = rank/len(edges)
+                for _, v in edges:
+                    # distribute the outgoing page rank to each node
+                    newpr_graph.nodes[v]["page_rank"] = newpr_graph.nodes[v].get("page_rank", 0) + shared_pr
+            # otherwise, let the current node keep all of its current rank
+            else:
+                newpr_graph.nodes[node]["page_rank"] = rank
+        
+        # TODO: some sort of check here if it's been a certain number of loops
+        #       if yes, then call `distribute()`
+
+    # store the results in the given output file
     store_pr(newpr_graph, output_file)
